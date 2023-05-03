90bac43284052024c97d90b6100ae7c9a8aac0bb
umairliaqat33 <umairliaqat552@gmail.com>
2023-05-03 16:28:12 +0500

    completed sir agha's django project 2 first part video

--- django/afterMids/afterMidsProject/afterMidsApp/models.py
+++ django/afterMids/afterMidsProject/afterMidsApp/models.py
@@ -7,9 +7,15 @@ class Department(models.Model):
     name = models.CharField(max_length=100, null=False)
     location = models.CharField(max_length=200)
 
+    def __str__(self):  # when using this function this will display name attribute on the tile of each object in admin panel
+        return self.name
+
 
 class Roles(models.Model):
     name = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Employee(models.Model):
@@ -21,3 +27,7 @@ class Employee(models.Model):
     role = models.ForeignKey(Roles, on_delete=models.CASCADE)
     phone = models.IntegerField(default=0)
     hire_date = models.DateField()
+
+    def __str__(self):
+        # using this we can print out more than one attributes
+        return "%s %s" % (self.firstName, self.lastName)
--- django/afterMids/afterMidsProject/afterMidsApp/urls.py
+++ django/afterMids/afterMidsProject/afterMidsApp/urls.py
@@ -3,4 +3,9 @@ from afterMidsApp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('view_all_emp', views.view_all_emp, name='view_all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('remove_emp', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp'),
+
 ]
